eea1deaa4934ff12ab1a96c4a10ea78c4460f046
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-26 11:33:14 +0530

    Demo2 final commit

--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,38 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Demo2 {
     public static void main(String[] args) {
-        
+        //   File file = new File("something.txt");
+
+        // try(FileInputStream fis = new FileInputStream(file)){
+
+        //     int read = -1;
+        //     byte[] buffer = new byte[1024];
+        //     while((read = fis.read(buffer)) != -1){
+        //         System.out.println(new String(buffer, 0,read));
+        //     }
+        //     System.out.println();
+        // }catch(IOException e){
+        //     e.printStackTrace();
+
+        // }
+
+         File file = new File("something.txt");
+
+        try(FileReader fis = new FileReader(file)){
+            int read = -1;
+            char[] buffer = new char[1024];
+            while((read = fis.read(buffer)) != -1){
+                System.out.println(new String(buffer, 0,read));
+            }
+            System.out.println();
+        }catch(IOException e){
+            e.printStackTrace();
+
+        }
+
     }
 }
