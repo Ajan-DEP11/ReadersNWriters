6402c57f44538efd4f68224e2765c1e4fe54f293
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-26 11:21:48 +0530

    Add second

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Demo1 {
     public static void main(String[] args) {
+         File file = new File("something.txt");
+
+        try(FileInputStream fis = new FileInputStream(file)){
+
+            int read = -1;
+            while((read = fis.read()) != -1){
+                System.out.println((char)read);
+            }
+            System.out.println();
+        }catch(IOException e){
+            e.printStackTrace();
+
+        }
+
+
+
+
         
     }
 }
