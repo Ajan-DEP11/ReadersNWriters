46fd164a806adb4203ee41c4a6dd984f30822d26
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-26 11:29:05 +0530

    Add Demo2

--- Demo1.java
+++ Demo1.java
@@ -1,12 +1,27 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Demo1 {
     public static void main(String[] args) {
-         File file = new File("something.txt");
+        //  File file = new File("something.txt");
 
-        try(FileInputStream fis = new FileInputStream(file)){
+        // try(FileInputStream fis = new FileInputStream(file)){
+
+        //     int read = -1;
+        //     while((read = fis.read()) != -1){
+        //         System.out.println((char)read);
+        //     }
+        //     System.out.println();
+        // }catch(IOException e){
+        //     e.printStackTrace();
+
+        // }
+
+          File file = new File("something.txt");
+
+        try(FileReader fis = new FileReader(file)){
 
             int read = -1;
             while((read = fis.read()) != -1){
@@ -17,6 +32,9 @@ public class Demo1 {
             e.printStackTrace();
 
         }
+
+
+
 
 
 
